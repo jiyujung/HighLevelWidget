7cbd33eae332c327d10df5ffe6ae2bd4bf53845a
jiyujung <s2019w39@e-mirim.hs.kr>
2020-11-03 19:13:47 +0900

    Complete ProgressBar, SeekBar, RatingBar

---
+++ app/src/main/java/kr/hs/emirim/s2019w39/highlevelwidget/MainActivity3.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2019w39.highlevelwidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
